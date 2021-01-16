466f3d76ab7e195e41bd71f87b57f34ffed61492
RediG7 <guleksiredi7@gmail.com>
2021-01-16 19:34:47 +0100

    Add functionality to client side

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,69 @@
 const chatForm = document.getElementById("chat-form");
-const chatMessages = document.querySelector('.chat-messages');
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
 const socket = io();
 
+// Join chatroom
+socket.emit("joinRoom", { username, room });
+
+// Get room and users
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoomName(room);
+  outPutUsers(users);
+});
+
 // Message from server
 socket.on("message", (message) => {
-    console.log(message);
-    outPutMessage(message);
+  console.log(message);
+  outPutMessage(message);
 
-    // Scroll down
-    chatMessages.scrollTop = chatMessages.scrollHeight;
+  // Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 // Message submit
 chatForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  // when you submit a form it automatically submits to a file and we want to stop that from happening
+  e.preventDefault();
 
-    // Get message text
-    // it should get the message from the text input and then log it
-    const msg = e.target.elements.msg.value;
+  // Get message text
+  // it should get the message from the text input and then log it (from client to server side)
+  const msg = e.target.elements.msg.value;
 
-    // Emit message to server
-    socket.emit("chatMessage", msg);
+  // Emit message to server
+  socket.emit("chatMessage", msg);
 
-    // Clear input
-    e.target.elements.msg.value = '';
-    e.target.elements.msg.focus();
+  // Clear input
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
 });
 
 // Output message to DOM
 function outPutMessage(message) {
-    const div = document.createElement("div");
-    div.classList.add("message");
-    div.innerHTML = ` <p class="meta">Brad <span>9:12pm</span></p>
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = ` <p class="meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
-       ${message}
+       ${message.text}
     </p>`;
-    document.querySelector('.chat-messages').appendChild(div);
+  document.querySelector(".chat-messages").appendChild(div);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+// Add users to DOM
+function outPutUsers(users) {
+  userList.innerHTML = `
+    ${users.map((user) => `<li>${user.username}</li>`).join('')}
+    `;
 }
